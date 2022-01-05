ed9e68fb973dbb78c7c5a6083969497b91c8cafd
Przemek <przemek.iskra96@gmail.com>
2022-01-05 10:07:26 +0100

    Updates

---
+++ precache-manifest.5a61e036dba4e97e030e3a1094f01786.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc961cb5f4ad3c0e056b7c35c4060dd7",
+    "url": "/TicketApp/index.html"
+  },
+  {
+    "revision": "91b0415e94d148d627e4",
+    "url": "/TicketApp/static/css/main.7e09a068.chunk.css"
+  },
+  {
+    "revision": "93f0d0a5d6b7a27dc142",
+    "url": "/TicketApp/static/js/2.eef6bce4.chunk.js"
+  },
+  {
+    "revision": "91b0415e94d148d627e4",
+    "url": "/TicketApp/static/js/main.8e258534.chunk.js"
+  },
+  {
+    "revision": "89e4f459c02b71fa5821",
+    "url": "/TicketApp/static/js/runtime~main.b6e98e56.js"
+  },
+  {
+    "revision": "5efeae11c2e17e6913c043f1a90deced",
+    "url": "/TicketApp/static/media/logomzk.5efeae11.png"
+  },
+  {
+    "revision": "feb2d7ab9628f5c818b22099c0cd9ed7",
+    "url": "/TicketApp/static/media/skycash.feb2d7ab.jpg"
+  }
+]);
