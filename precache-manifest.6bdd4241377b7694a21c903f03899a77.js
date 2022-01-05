aede2fcff18c1fadc0de09c999c2d4bc28dbe293
Przemek <przemek.iskra96@gmail.com>
2022-01-05 10:58:57 +0100

    Updates

---
+++ precache-manifest.6bdd4241377b7694a21c903f03899a77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54813cec827a5f8004ae6d8342be8d88",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08694c8936b8c57cbd5d",
+    "url": "./static/css/main.7e09a068.chunk.css"
+  },
+  {
+    "revision": "072d98cd710b6263f087",
+    "url": "./static/js/2.2f75bb21.chunk.js"
+  },
+  {
+    "revision": "08694c8936b8c57cbd5d",
+    "url": "./static/js/main.b0cdc3a9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5efeae11c2e17e6913c043f1a90deced",
+    "url": "./static/media/logomzk.5efeae11.png"
+  },
+  {
+    "revision": "feb2d7ab9628f5c818b22099c0cd9ed7",
+    "url": "./static/media/skycash.feb2d7ab.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TicketApp/precache-manifest.776be724166e5a620cd878aa47784e78.js"
+  "./precache-manifest.6bdd4241377b7694a21c903f03899a77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TicketApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
